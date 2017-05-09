f25019c84fff3262ec37095062d3b9843567961c
peterpandayang <byang14@dons.usfca.edu>
2017-05-08 20:43:41 -0700

    asd

--- pa.c
+++ pa.c
@@ -1,10 +1,12 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
 
 #define process_count 3
 #define param_count 3
