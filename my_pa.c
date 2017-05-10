c128e23b3d15676ce8b3c34ac7ece23746b7605b
peterpandayang <byang14@dons.usfca.edu>
2017-05-10 16:55:40 -0700

    asdf

--- my_pa.c
+++ my_pa.c
@@ -146,6 +146,16 @@ int check_asc(char* read_buf){
     return result;
 }
 
+void print_buf(char* read_buf){
+    int i = 0;
+    int len = strlen(read_buf);
+    // write(2, &len, 1);
+    for(i = 0; i < len & &read_buf[i] != '\0'; i++){
+        write(2, &read_buf[i], 1);
+    }
+    read_buf[len] = '\0';
+}
+
 int write_to_file(struct value_st *input, char* read_buf, int index, char* cmd){
     int bytes, lines;
     char* bytes_str;
@@ -153,11 +163,16 @@ int write_to_file(struct value_st *input, char* read_buf, int index, char* cmd){
     char* index_str;
     char write_buf[1024];
     int is_asc = 0;
+    // int fds[2];
+
+    print_buf(read_buf);
 
     bytes = strlen(read_buf);
     // write(2, &bytes, 2);
     // write(2, &bytes, 2);
     bytes_str = itoa(bytes);
+    write(2, bytes_str, 2);
+    write(2, bytes_str, 2);
     lines = count_lines(read_buf, bytes);
     lines_str = itoa(lines);
     is_asc = check_asc(read_buf);
@@ -264,15 +279,10 @@ int do_with_one_pipe(struct value_st *input){
         close(pipe_1_m1[1]);
         close(0);
         dup(pipe_1_m1[0]);
-        int bytes;
-        int lines;
-        // printf("line is: %d\n", lines);
-        if((bytes = read(0, read_buf, buf_size)) < 0) {
+        if(read(0, read_buf, 64) < 0) {
             write(2, "cannot read from pipe\n", 23);
             exit(-1);
         }
-        printf("bytes is: %s\n", bytes);
-        // lines = count_lines(read_buf, strlen(read_buf));
 
         close(1);
         dup(pipe_m1_p[1]);
@@ -327,8 +337,6 @@ int do_with_one_pipe(struct value_st *input){
     dup(pipe_m1_p[0]);
     close(pipe_m1_p[0]);
 
-    // int byte = read(0, read_buf, 64);
-    // printf("byte is: %d\n", byte);
     if(read(0, read_buf, 64) < 0) {
         write(2, "cannot read from pipe\n", 23);
         exit(-1);
