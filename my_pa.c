ef2f70ecedd8fdd6aa5ea6aa6d9ed2b7507c4993
peterpandayang <byang14@dons.usfca.edu>
2017-05-10 01:46:14 -0700

    asdf

--- my_pa.c
+++ my_pa.c
@@ -134,7 +134,7 @@ int check_asc(char* read_buf){
     int i = 0;
     int result = 1;
     for(i = 0; i < size; i++){
-        if(read_buf[i] - '0' > 127){
+        if(read_buf[i] - '0' > 127 && read_buf[i] != '?'){
             printf("char: %d\n", read_buf[i] - '0');
             printf("char: %c\n", read_buf[i]);
             result = 0;
